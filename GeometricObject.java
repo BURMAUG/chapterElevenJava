a396f90a3e22ce2ad763e00f05396602af256399
Deejai <burmaud1@gmail.com>
2020-08-09 05:08:22 -0400

    class of GeometricObject

---
+++ GeometricObject.java
@@ -0,0 +1,40 @@
+public class GeometricObject{
+  String color;
+  boolean filled;
+  java.util.Date  dateCreate;
+
+  //empty constructor 
+  GeometricObject(){};
+  //constructor
+  GeometricObject(String color, boolean filled){
+    this.color = color;
+    this.filled = filled;
+  };
+  //get the color 
+  public String getColor(){
+    return color;
+  };
+  //set the color 
+  public void setColor(String color){
+    this.color = color;
+  };
+  //boolean for wether or not its filled
+  public boolean isFilled(){
+    return filled;
+  };
+
+  //set a new filled boolean prop
+  public void setFilled(boolean filled){
+    this.filled = filled;
+  };
+
+  //Date 
+  public Java.util.Date getCreated(){
+    return dateCreated;
+  };
+  //turn this to understandable string
+  public String toString(){
+    return "Date "  + dateCreate + "\nColor: " + color + "\nFilled " + filled;
+  };
+
+}
